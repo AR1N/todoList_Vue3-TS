f6f68de023aff884802bd4df8b60446449dd9166
aren <739190475@qq.com>
2020-11-27 16:18:00 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,73 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="root">
+        <img alt="Vue logo" src="./assets/logo.png" />
+        <TopInput tit='vue3.0+ts' @onAdd='addData'/>
+        <div style="margin-top:50px">
+          <div class="item" v-for="(item,index) in todo" :key="index">{{item}} <span class="el-icon-circle-close del" @click="onDel(index)"></span> </div>
+        </div> 
+    </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import { defineComponent,reactive,toRefs } from "vue";
+import TopInput from "./components/topInput.vue";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+export default defineComponent({
+    name: "App",
+    components: {
+        TopInput
+    },
+    setup(){
+        let list:any[string] = reactive({
+          todo:[]
+        })
+      function onDel(index:number){
+        list.todo.splice(index,1)
+      }
+      function addData(data:any){
+       list.todo.unshift(data)
+      }
+      return {
+        ...toRefs(list),
+        onDel,
+        addData
+      }
+    }
+});
 </script>
 
-<style>
+<style >
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+.root{
+ max-width: 740px;
+ margin:50px auto;
+}
+.item{
+  position: relative;
+  color: #41b883;
+  font-weight: bold;
+  padding: 20px 60px 20px;
+  border-radius: 10px;
+  margin-bottom: 15px;
+word-break: break-all;
+  background-color: #fff;
+  box-shadow: 0 5px 10px #35495e;
+}
+.del{
+  position: absolute;
+  right: 20px;
+  top: 20px;
+  font-size: 20px;
+  color: red;
+  cursor: pointer;
 }
 </style>
+
+
---
+++ src/components/topInput.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="hello">
+        <h1>{{tit}}</h1>
+        <div class="flex-align">
+            <el-input
+                placeholder="请输入内容"
+                prefix-icon="el-icon-edit"
+                v-model="inputVal"
+                clearable
+            >
+            </el-input>
+            <el-button type='primary' style="margin-left:20px;width:100px" @click="onAdd">添加</el-button>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+    name: "topInput",
+    props: {
+        tit: String,
+    },
+    setup() {
+        return {
+            inputVal: ref(""),
+        };
+    },
+    methods:{
+        onAdd(){
+            if(!this.inputVal){
+                return
+            }
+            this.$emit('on-add',this.inputVal)
+            this.inputVal = ''
+        }
+    }
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.flex-align {
+    display: flex;
+    align-items: center;
+}
+</style>
